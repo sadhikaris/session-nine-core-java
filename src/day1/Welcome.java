a202861d74748d231e662dddb08e9a513656e2aa
sadhikaris <87678158+sadhikaris@users.noreply.github.com>
2023-12-28 01:48:27 -0600

    Initial commit

---
+++ src/day1/Welcome.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Welcome {
+    public static void main(String[] args){
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting.length());
+        System.out.println(greeting);
+        for(int i =0; i < greeting.length(); i ++){
+            System.out.println("=");
+        }
+        System.out.println();
+
+    }
+}
