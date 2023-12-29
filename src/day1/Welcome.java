43e24743d0d93eb50874eb58a449bb6633babcd4
sadhikaris <87678158+sadhikaris@users.noreply.github.com>
2023-12-29 01:07:36 -0600

    fixed m

--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -6,7 +6,7 @@ public class Welcome {
         System.out.println(greeting.length());
         System.out.println(greeting);
         for(int i =0; i < greeting.length(); i ++){
-            System.out.println("=");jj
+            System.out.println("=");
         }
         System.out.println();
 
